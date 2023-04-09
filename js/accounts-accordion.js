574755d0bb5e38571388f6233f313b5e114839dd
Samedusa4real <samadnm@code.edu.az>
2023-04-09 13:07:30 -0800

    day4 ends and 4pages completed alhamdulillah!!

--- js/accounts-accordion.js
+++ js/accounts-accordion.js
@@ -1,52 +1,86 @@
-const itemButtons = document.querySelectorAll(".accordion-item .accordion-header i")
-// const minusButtons = document.querySelectorAll(".accordion-item .accordion-header i:last-child")
-const itemContents = document.querySelectorAll(".accordion-item .accordion-content")
-const accordionHeader = document.querySelector(".accordion-item .accordion-header p")
+// const itemButtons = document.querySelectorAll(".accordion-item .accordion-header i")
+// // const minusButtons = document.querySelectorAll(".accordion-item .accordion-header i:last-child")
+// const itemContents = document.querySelectorAll(".accordion-item .accordion-content")
+// const accordionHeader = document.querySelector(".accordion-item .accordion-header p")
 
 
-itemButtons.forEach(itemButton => {
-    const minusButton = itemButton.nextElementSibling
-    itemButton.addEventListener("click", function () {
-        const item = itemButton.parentNode.parentNode
-        const accordionItems = Array.from(item.parentNode.children)
+// itemButtons.forEach(itemButton => {
+//     const minusButton = itemButton.nextElementSibling
+//     itemButton.addEventListener("click", function () {
+//         const item = itemButton.parentNode.parentNode
+//         const accordionItems = Array.from(item.parentNode.children)
 
-        item.classList.toggle("active")
+//         item.classList.toggle("active")
 
         
-        // if(minusButton.classList.contains("d-none")){
-        //     minusButton.classList.remove("d-none")
-        //     itemButton.classList.add("d-none")
-        // }
+//         // if(minusButton.classList.contains("d-none")){
+//         //     minusButton.classList.remove("d-none")
+//         //     itemButton.classList.add("d-none")
+//         // }
 
 
-        accordionItems.forEach(accordionItem => {
-            if(item.classList.contains("active")){
-                console.log(accordionText);
-                accordionHeader.style.color = "#099a71"
-            }
-            else{
-                accordionText.style.color = "purple"
+//         accordionItems.forEach(accordionItem => {
+//             if(item.classList.contains("active")){
+//                 console.log(accordionText);
+//                 accordionHeader.style.color = "#099a71"
+//             }
+//             else{
+//                 accordionText.style.color = "purple"
 
-            }
-            if (!accordionItem.classList.contains("active")) {
-                // console.log(accordionItem);
-                accordionText.style.display = "none"
-            }
-            else{
-                accordionText.style.display = "block"
-                accordionHeader.style.color = "#099a71"
-                //     minusButton.classList.remove("d-none")
-                // itemButton.classList.add("d-none")
-                // minusButton.classList.remove("d-none")
-                // itemButton.classList.add("d-none")
+//             }
+//             if (!accordionItem.classList.contains("active")) {
+//                 // console.log(accordionItem);
+//                 accordionText.style.display = "none"
+//             }
+//             else{
+//                 accordionText.style.display = "block"
+//                 accordionHeader.style.color = "#099a71"
+//                 //     minusButton.classList.remove("d-none")
+//                 // itemButton.classList.add("d-none")
+//                 // minusButton.classList.remove("d-none")
+//                 // itemButton.classList.add("d-none")
                 
                 
-            }
-            accordionItem.classList.remove("active")
-            // accordionHeader.style.color = "#212529"
+//             }
+//             accordionItem.classList.remove("active")
+//             // accordionHeader.style.color = "#212529"
             
-        })
+//         })
 
         
+//     })
+// })
+
+const accordionItems = document.querySelectorAll(".accordion-item")
+
+accordionItems.forEach((accordionItem,index) => {
+    let header = accordionItem.querySelector(".accordion-header")
+    header.addEventListener("click", function(){
+        accordionItem.classList.toggle("open")
+
+        let description = accordionItem.querySelector(".accordion-content")
+        if(accordionItem.classList.contains("open")){
+            description.style.display = "block"
+            header.querySelector("i").classList.replace("fa-plus", "fa-minus")
+            header.style.color = "green"
+        }
+        else{
+            description.style.display = "none"
+            header.querySelector("i").classList.replace("fa-minus", "fa-plus")
+
+        }
+        removeOpen(index)
     })
 })
+
+function removeOpen(index1){
+    accordionItems.forEach((item2, index2) =>{
+        if(index1 !== index2){
+            item2.classList.remove("open");
+
+            let des = item2.querySelector(".accordion-content")
+            des.style.display = "none"
+            item2.querySelector("i").classList.replace("fa-minus", "fa-plus")
+        }
+    })
+}
---
+++ js/salary-tab.js
@@ -0,0 +1,29 @@
+const tabButtons = document.querySelectorAll("#salary-benefits-section .nav-tabs button")
+
+const firstContent = document.querySelector("#tab1")
+const secondContent = document.querySelector("#tab2")
+
+
+tabButtons.forEach(tabButton => {
+    const tab = tabButton.getAttribute("data-tab")
+    console.log(tab);
+    tabButton.addEventListener("click", function(){
+        if(tab === firstContent.id){
+            firstContent.classList.add("show")
+            secondContent.classList.remove("show")
+            tabButton.classList.add("active")
+            tabButton.nextElementSibling.classList.remove("active")
+            tabButton.style.color = "#343836"
+            tabButton.nextElementSibling.style.color = "#00000061"
+        }
+        else if(tab === secondContent.id){
+            firstContent.classList.remove("show")
+            secondContent.classList.add("show")
+            tabButton.classList.add("active")
+            tabButton.previousElementSibling.classList.remove("active")
+            tabButton.style.color = "#343836"
+            tabButton.previousElementSibling.style.color = "#00000061"
+        }
+
+    })
+})
